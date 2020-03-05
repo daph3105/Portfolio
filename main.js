a93c474ad0f0ed394f41c66e3d665a12ab55e276
Daphne <daphne.feresin@gmail.com>
2020-03-05 18:04:04 -0500

    skills animation

--- main.js
+++ main.js
@@ -22,3 +22,26 @@ profileImg.onclick = function(){
     profileImg.src = "images/daph-profile.gif" 
     setTimeout(function(){ profileImg.src = "images/daph-profile.jpg" }, 3000); 
 };
+
+function debounce(func, wait = 20, immediate = true) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+const sliderImages = document.querySelectorAll('.slide-in')
+
+function checkSlide (e){
+console.log(e)
+}
+
+window.addEventListener('scroll', checkSlide)
