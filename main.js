2a2561d2ffcbee3b6fb8374eeeb34a4706b17baf
Daphne <daphne.feresin@gmail.com>
2020-03-16 18:52:07 -0400

    skills

--- main.js
+++ main.js
@@ -43,9 +43,9 @@ const img1 = document.getElementById("img1")
 const img2 = document.getElementById("img2")
 const img3 = document.getElementById("img3")
 const img4 = document.getElementById("img4")
+const img5 = document.getElementById("img5")
 
-
-let images = [img1,img2,img3,img4]
+let images = [img1,img2,img3,img4,img5]
 
 function checkSlide (e){
     let number = 2;
